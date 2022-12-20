a755d352b0247a7418b334db781347f698c12754
Bart Massey <bart.massey@gmail.com>
2022-12-20 15:00:45 -0800

    documentation for fc92cb262c4a032cf7580c5e5ef5bd442879e0a6 from main

---
+++ docs/doc/implementors/mut_elems/trait.AsMutElemsVecExt.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mut_elems":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
